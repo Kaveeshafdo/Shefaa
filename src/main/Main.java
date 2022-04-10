364c8e100119b9d30cc4e02da8c530afed768b77
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-04-10 20:14:48 +0530

    Create main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,79 +12,123 @@ import models.Patient;
  *
  * @author Kaveesha FDO
  */
-public class Main extends ShefaAdminApp {
+class Menu {
+
+    private int num;
+
+    public void setMenu(int num) {
+        this.num = num;
+    }
+
+    public int getMenu() {
+        return this.num;
+    }
+}
+
+public class Main {
+
+    static Patient patient = new Patient();
 
     public static void main(String[] args) {
 
         ShefaAdminApp shefaa = new ShefaAdminApp();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter option number");
-        System.out.println("(1) - Patient");
-        System.out.println("(2) - Service");
-        System.out.println("(3) - Slot");
-        int optionNumber = sc.nextInt();
+        Menu menu = new Menu();
+        do {
+            Scanner sc = new Scanner(System.in);
 
-        switch (optionNumber) {
-            case 1:
-//                Scanner sc = new Scanner();
-                System.out.println("Please enter suboption number");
-                System.out.println("(1) - Add Patient");
-                System.out.println("(2) - Delete Patient");
-                System.out.println("(3) - Find Patient");
-                System.out.println("(4) - Modify Patient");
-                System.out.println("(5) - Back");
-                int patientSubOptionNumber = sc.nextInt();
+            System.out.println(
+                    "Please enter option number");
+            System.out.println(
+                    "(1) - Patient");
+            System.out.println(
+                    "(2) - Service");
+            System.out.println(
+                    "(3) - Slot");
+            System.out.println(
+                    "(4) - Exit");
+            int optionNumber = sc.nextInt();
+            menu.setMenu(optionNumber);
+            switch (menu.getMenu()) {
+                case 1:
 
-                switch (patientSubOptionNumber) {
-                    case 1:
-                     
-                        Patient p = new Patient(1,"Kaveesha","VISITOR");                      
-                        shefaa.addPatient(p);
-                        shefaa.showAllPatients();
+                    System.out.println("Please enter suboption number");
+                    System.out.println("(1) - Add Patient");
+                    System.out.println("(2) - Delete Patient");
+                    System.out.println("(3) - Find Patient");
+                    System.out.println("(4) - Modify Patient");
+                    System.out.println("(5) - Show all Patient");
+                   
+                    int patientSubOptionNumber = sc.nextInt();
 
-                        break;
+                    switch (patientSubOptionNumber) {
+                        case 1:
 
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                }
-                break;
-            case 2:
-                System.out.println("Please enter suboption number");
-                System.out.println("(1) - Add Service");
-                System.out.println("(2) - Delete Service");
-                System.out.println("(3) - Find Service");
-                System.out.println("(4) - Modify Service");
-                System.out.println("(5) - Back");
-                int ServiceSubOptionNumber = sc.nextInt();
+                            System.out.println("Please Enter Patient Id");
+                            int patientIdNumber = sc.nextInt();
+                            System.out.println("Please Enter Patient Name");
+                            String patientName = sc.next();
+                            System.out.println("Please Enter Patient ResidanceType Number");
+                            System.out.println("(1) - VISITOR");
+                            System.out.println("(2) - RESIDENT");
+                            int residanceTypeNumber = sc.nextInt();
+                            if (residanceTypeNumber == 1) {
+                                patient.setResidanceType("VISITOR");
+                            } else {
+                                patient.setResidanceType("RESIDENT");
+                            }
 
-                switch (ServiceSubOptionNumber) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                }
-                break;
-            case 3:
-                System.out.println("Please enter suboption number");
-                System.out.println("(1) - Add Patient");
-                System.out.println("(2) - Delete Patient");
-                System.out.println("(3) - Find Patient");
-                System.out.println("(4) - Modify Patient");
-                System.out.println("(5) - Back");
-                break;
-        }
+                            Patient p = new Patient(patientIdNumber, patientName, patient.getResidanceType());
+                            System.out.println(shefaa.addPatient(p));
 
+                        case 2:
+                            shefaa.deletePatient(1);
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            shefaa.showAllPatients();
+                            break;
+                       
+                    }
+                    break;
+                case 2:
+                    System.out.println("Please enter suboption number");
+                    System.out.println("(1) - Add Service");
+                    System.out.println("(2) - Delete Service");
+                    System.out.println("(3) - Find Service");
+                    System.out.println("(4) - Modify Service");
+                    System.out.println("(5) - Back");
+                    int ServiceSubOptionNumber = sc.nextInt();
+
+                    switch (ServiceSubOptionNumber) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Please enter suboption number");
+                    System.out.println("(1) - Add Patient");
+                    System.out.println("(2) - Delete Patient");
+                    System.out.println("(3) - Find Patient");
+                    System.out.println("(4) - Modify Patient");
+                    System.out.println("(5) - Back");
+                    break;
+                    
+                case 4:
+                    break;
+            }
+        } while (menu.getMenu() != 4);
+        
     }
+
 }
